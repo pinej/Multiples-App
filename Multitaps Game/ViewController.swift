0d5aedc370f797d3c188a4510bdf7a8b45b9a2e7
pinej <piotr.nejman@gmail.com>
2015-12-27 22:41:19 +0100

    Working app

--- Multitaps Game/ViewController.swift
+++ Multitaps Game/ViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Properies
+    var currentAdds = 0
+    var whatToAdd = 0
+    var numA = 0
+    
+    var result = 0
+    
+    //Outlets
+    @IBOutlet weak var logo: UIImageView!
+
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var whatAddTxt: UITextField!
+    
+    @IBOutlet weak var addLbl: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +38,70 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onPlayBtnPressed(sender: AnyObject) {
 
+        
+        if whatAddTxt.text != nil && whatAddTxt.text != "" {
+            logo.hidden = true
+            playBtn.hidden = true
+            whatAddTxt.hidden = true
+            
+            addBtn.hidden = false
+            addLbl.hidden = false
+            addLbl.text = "Press ADD to add"
+
+            
+          whatToAdd = Int(whatAddTxt.text!)!
+         
+            currentAdds = 0
+            
+        }
+    }
+
+ 
+    @IBAction func onAddBtn(sender: AnyObject) {
+        
+        updateAddsLabel()
+        if gameOver() {
+            restartGame()
+        }
+        
+        
+        
+    }
+    
+    func updateAddsLabel() {
+
+    
+        result = currentAdds * whatToAdd
+        
+        addLbl.text = "\(result) + \(whatToAdd) = \((result) + (whatToAdd))"
+        
+        currentAdds++
+
+        
+    }
+    
+    func restartGame() {
+        whatAddTxt.text = ""
+        logo.hidden = false
+        playBtn.hidden = false
+        whatAddTxt.hidden = false
+        
+        addBtn.hidden = true
+        addLbl.hidden = true
+        
+        currentAdds = 0
+    }
+    
+    func gameOver() -> Bool{
+        if currentAdds >= 10 {
+            return true
+        } else {
+            return false
+            
+        }
+    }
+    
 }
 
